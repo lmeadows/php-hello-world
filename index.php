1f5588b31a9c1a1d741e8b884510134a32dbd2f4
lmeadows <meadows.lucas@gmail.com>
2017-10-26 16:54:16 -0700

    adding hello world file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo <<<CONTENT
+<html>
+<head>
+<style type="text/css">
+body {
+  background-color:#89cff0;
+}
+p {
+  font-weight:bold;
+}
+</style>
+</head>
+<body>
+<p>Hello, world!</p>
+</body>
+</html>
+CONTENT;
+?>
